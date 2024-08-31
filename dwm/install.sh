245a19fcd2403e952ec85bdd1309cc211a58da3c
Alejandro Lazarte <chealote@gmail.com>
2024-08-31 17:17:08 -0300

    dwm custom

---
+++ dwm/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+if [ -d ./dwm/ ]; then
+	read -p "delete the folder first? (y/n): " option
+	[[ "$option" != "y" ]] && exit 1
+	rm -rf ./dwm/
+fi
+
+git clone --depth 1 https://git.suckless.org/dwm
+
+cp *diff dwm/
+cd dwm/
+
+for patchfile in $(ls *diff); do
+	patch -p1 < $patchfile
+done
+
+make
+# you can `make install` now or `ln -s` the binary, or add this path to $PATH
